12718c11e3c85f1b1f1a3fb794e9c2869c0fefab
Niklas Molnes Hole <niklas.hole@bekk.no>
2022-03-27 20:11:02 +0200

    Add list of things i eat on a daily basis
    Yup, I know this is private info :P

--- src/App.js
+++ src/App.js
@@ -1,7 +1,77 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+let initialList = [];
+try {
+  initialList = JSON.parse(localStorage.getItem("list")) || [];
+} catch (e) {}
+
 function App() {
-  return <div className="App">RPi Touch GUI</div>;
+  const [list, setList] = useState(initialList);
+
+  // Yup, this is what I eat on a daily basis
+  const [stuff] = useState(
+    `
+kjøttdeig
+biff
+karbonadedeig
+yoghurt
+bolognese
+billys
+tikka
+risgrøt
+macncheese
+tikkaboks
+cornflakes
+bixit
+naanbrød
+smør
+tomat
+løk
+sopp
+blåbærsyltetøy
+bringebærsyltetøy
+rundstykker
+grandis
+melk
+nudler
+fries
+saft
+brus
+øl
+vin
+mellombar
+kylling
+tacoshell
+nachos
+`
+      .trim()
+      .split("\n")
+  );
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <div className="App">
+      <button onClick={() => window.location.reload()}>Refresh</button>
+      <br />
+      {stuff.map((s) => (
+        <button key={s} onClick={() => setList((l) => [...l, { name: s, id: Date.now(), date: Date.now() }])}>
+          {s}
+        </button>
+      ))}
+      <br />
+      {list
+        .sort((a, b) => a.date - b.date)
+        .map((e) => (
+          <div key={e.name} onClick={() => setList((l) => l.filter((i) => i.id !== e.id))}>
+            {e.name}
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default App;
