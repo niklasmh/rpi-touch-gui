c01646dec9c18af1d1ce83b91a370a12f6823960
Niklas Molnes Hole <nikasmh@hotmail.com>
2022-03-28 22:27:56 +0200

    Add some more functionality

--- src/App.js
+++ src/App.js
@@ -1,70 +1,97 @@
 import { useEffect, useState } from "react";
 
 let initialList = [];
+let initialZoom = 1;
 try {
   initialList = JSON.parse(localStorage.getItem("list")) || [];
+  initialZoom = JSON.parse(localStorage.getItem("zoom")) || 1;
 } catch (e) {}
 
 function App() {
   const [list, setList] = useState(initialList);
+  const [cursorShouldHide, hideCursor] = useState(false);
+  const [zoom, setZoom] = useState(initialZoom);
 
   // Yup, this is what I eat on a daily basis
   const [stuff] = useState(
     `
-kjøttdeig
-biff
-karbonadedeig
-yoghurt
-bolognese
-billys
-tikka
-risgrøt
-macncheese
-tikkaboks
-cornflakes
-bixit
-naanbrød
-smør
-tomat
-løk
-sopp
-blåbærsyltetøy
-bringebærsyltetøy
-rundstykker
-grandis
-melk
-nudler
-saft
-mellombar
-kylling
-nachos
-mais(åpnet)
+--- Frokost
+yoghurt 🍨 10
+cornflakes 🌽
+bixit 🍪
+rundstykker 🥖
+blåbærsyltetøy 🍓
+bringebærsyltetøy 🍓
+smør 🧈 50
+--- Brunch
+bolognese 🍝
+nachos 🌮
+risgrøt 🥣 10
+mellombar 🍫
+billys 🍕
+tikka 🥡
+macncheese 🧀
+nudler 🍜
+melk 🥛 20
+--- Middag
+kjøttdeig 🥩 10
+karbonadedeig 🥩 10
+biff 🥩 10
+kylling 🍗
+grandis 🍕
+mais(åpnet) 🌽 2
+sopp 🍄 8
+tomat 🍅 3
+løk 🧅 15
+gulerøtter 🥕 20
+naanbrød 🍞 20
 `
       .trim()
       .split("\n")
+      .map((item) => {
+        const [name, emoji = "", days = "100"] = item.split(" ");
+        const time = parseInt(days) * 24 * 60 * 60 * 1000;
+        return { name, emoji, time };
+      })
   );
 
   useEffect(() => {
     localStorage.setItem("list", JSON.stringify(list));
   }, [list]);
 
+  useEffect(() => {
+    localStorage.setItem("zoom", JSON.stringify(zoom));
+  }, [zoom]);
+
   return (
-    <div className="App">
+    <div className="App" style={{ zoom, cursor: cursorShouldHide ? "none" : "default" }}>
       <div>
-        <button onClick={() => document.documentElement.requestFullscreen()}>Fullscreen</button>
-        <button onClick={() => window.location.reload()}>Refresh</button>
-        {stuff.map((s) => (
-          <button key={s} onClick={() => setList((l) => [...l, { name: s, id: Date.now(), date: Date.now() }])}>
-            {s}
-          </button>
-        ))}
+        <button onClick={() => document.documentElement.requestFullscreen()}>Fullscreen ⏹</button>
+        <button onClick={() => window.location.reload()}>Refresh 🔃</button>
+        <button onClick={() => hideCursor((hide) => !hide)}>{cursorShouldHide ? "Show" : "Hide"} cursor 🖱</button>
+        <button onClick={() => setZoom((zoom) => zoom + 0.1)}>Zoom +</button>
+        <button onClick={() => setZoom((zoom) => zoom - 0.1)}>Zoom -</button>
+        {stuff.map(({ name, emoji, time }) =>
+          name === "---" ? (
+            <div key={emoji} className="gap">
+              {emoji}
+            </div>
+          ) : (
+            <button
+              key={name}
+              style={{ cursor: cursorShouldHide ? "none" : "default" }}
+              onClick={() => setList((l) => [...l, { name, emoji, id: Date.now(), expires: Date.now() + time }])}
+            >
+              {name} {emoji}
+            </button>
+          )
+        )}
       </div>
       <div className="items">
         {Object.values(
           list
-            .sort((a, b) => b.date - a.date)
+            .sort((a, b) => b.expires - a.expires)
             .reduce((acc, e) => {
-              console.log(acc);
               return {
                 ...acc,
                 [e.name]: {
@@ -75,10 +102,10 @@ mais(åpnet)
               };
             }, {})
         )
-          .sort((a, b) => a.date - b.date)
+          .sort((a, b) => a.expires - b.expires)
           .map((e) => (
             <div key={e.name} onClick={e.onClick} className="item">
-              {e.name} ({e.count})
+              {e.name} {e.emoji} ({e.count})
             </div>
           ))}
       </div>
